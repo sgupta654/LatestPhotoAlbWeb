242c4fd699cb73108e99cd9f7227d00c4122a905
chenan02 <chenan02@gmail.dom>
2015-10-23 14:51:03 -0400

    caption.js

---
+++ static/js/caption.js
@@ -0,0 +1,60 @@
+// caption.js
+function Caption(element, picid, caption) {
+  this.element = element;
+  this.picid = picid;
+  element.value = caption; // objects in Javascript are assigned by reference, so this works
+  element.addEventListener("change", this, false); 
+}
+
+Caption.prototype.handleEvent = function(e) {
+  if (e.type === "change") {
+    this.update(this.element.value);
+  }
+}
+
+Caption.prototype.change = function(value) {
+  this.data = value;
+  this.element.value = value;
+}
+
+Caption.prototype.update = function(caption) {
+  makeCaptionPostRequest(this.picid, caption, function() {
+    console.log('POST successful.');
+  });
+}
+
+function makeCaptionRequest(picid, cb) {
+  qwest.get('/secretkey/pa3/pic/caption?id=' + picid)
+    .then(function(xhr, resp) {
+      cb(resp);
+    });
+}
+
+function makeCaptionPostRequest(picid, caption, cb) {
+  var data = {
+    'id': picid,
+    'caption': caption
+  };
+
+  qwest.post('/secretkey/pa3/pic/caption', data, {
+    dataType: 'json',
+    responseType: 'json'
+  }).then(function(xhr, resp) {
+    cb(resp);
+  });
+}
+
+function initCaption(picid) {
+  var caption = document.getElementById("caption");
+  var captionBinding = new Caption(caption, picid);
+
+  makeCaptionRequest(picid, function(resp) {
+    captionBinding.change(resp['caption']);
+  });
+
+  setInterval(function() {
+   makeCaptionRequest(picid, function(resp) {
+      captionBinding.change(resp['caption']);
+    }); 
+  }, 7000);
+}
